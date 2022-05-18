5500612f007281d6b410c45d6fea608b208618a9
Angelika Przeliorz <artgelika@gmail.com>
2022-05-18 21:49:01 +0200

    Improve loading photos method

--- LoadingData.py
+++ LoadingData.py
@@ -1,13 +1,15 @@
 import os
-import cv2            
+import cv2
 from tqdm import tqdm
-from sklearn.utils import shuffle 
-import numpy as np  
+from sklearn.utils import shuffle
+import numpy as np
+import pickle
+
 
 class LoadingPhotos:
     def __init__(self, name):
         self.name = name
-        self.X_set, self.y_set, self.set_of_data = [],[],[]
+        self.X_set, self.y_set, self.set_of_data = [], [], []
 
     def loading(self):
         self._correct_path()
@@ -18,30 +20,32 @@ class LoadingPhotos:
 
     def _correct_path(self):
         self.path = PATH + "\\" + self.name
-        return self.path 
+        return self.path
 
     def _load_data(self):
         try:
-            self.dataset = os.listdir(self.path)  
+            self.dataset = os.listdir(self.path)
             return self.dataset
         except FileNotFoundError:
-            print("File not exist") 
+            print("File not exist")
 
-    def _label_creator(self, img): 
-        word_label = img[0] 
+    def _label_creator(self, img):
+        word_label = img[0]
         assert len(self.specific_letters) == 3
         numbers_spec = [i for i in range(len(self.specific_letters))]
 
         for i in range(len(self.specific_letters)):
-            if word_label == self.specific_letters[i]: return numbers_spec[i] 
-    
+            if word_label == self.specific_letters[i]:
+                return numbers_spec[i]
+
     def _create_set_data(self):
-        self.specific_letters = list(set([x[0] for x in self.dataset])) 
+        self.specific_letters = list(set([x[0] for x in self.dataset]))
         for img in tqdm(self.dataset):
             try:
-                label = self._label_creator(img) 
+                label = self._label_creator(img)
                 path = os.path.join(self.path, img)
-                img = cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), IMG_SIZE)
+                img = cv2.resize(cv2.imread(
+                    path, cv2.IMREAD_GRAYSCALE), IMG_SIZE)
                 self.set_of_data.append([np.array(img), np.array(label)])
 
             except Exception as e:
